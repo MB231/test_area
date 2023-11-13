6af0aa64b9e0db64c8dd6da42db907a824f1e88a
Matthew Barnekow <matthew.barnekow@fuseintegration.com>
2023-11-12 19:28:52 -0800

    Moved all test dir to respective build tools

--- shimLibEx/abiSharedLib/lib2/newLib.cpp
+++
@@ -1,9 +0,0 @@
-
-// ensures C linkage and not C++ which will mangle the name
-extern "C" {
-    int rand();
-}
-
-int rand() {
-    return 888;
-}
--- template_specialization_class_member/classTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "classTest.hpp"
-#include <iostream>
-
-template<>
-void templatedClass<ethDFPort>::read() {
-    std::cout << "eth templated func called"<< std::endl;
-}
-
